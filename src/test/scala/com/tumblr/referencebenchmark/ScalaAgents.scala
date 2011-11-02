9332e36735c89b59ba3d5c935fbbaf24bfb4d890
Wiktor Macura <wiktor@tumblr.com>
2011-11-02 06:04:23 -0600

    Trivial reference benchmark on actor receive vs. react:
    Time in ms per 1000 blocking requests over 50 benchmarks:
    
    	blocking_react->
    		Min = 16
    		Mean = 18.78
    		Max = 30
    
    	blocking_receive->
    		Min = 16
    		Mean = 17.94
    		Max = 27

--- src/test/scala/com/tumblr/referencebenchmark/ScalaAgents.scala
+++ src/test/scala/com/tumblr/referencebenchmark/ScalaAgents.scala
@@ -5,7 +5,12 @@ import scala.actors.Actor._
 
 class ScalaAgents
 	extends SpecWithSpecsbenchReporting
-{		
+{
+  report(
+    metrics("blocking_receive", Seq(min, mean, max)),
+    metrics("blocking_react", Seq(min, mean, max))
+  )
+
 	"Agents" should {
 		case class Message(val number: Int)
 		case class Reply(val number: Int)
@@ -21,9 +26,36 @@ class ScalaAgents
 					}
 				}
 			}
+
+      statTiming("blocking_receive", 50) {
+        var index = 0
+        while(index < 1000) {
+          Receiver.processor !? Message(index)
+          index += 1
+        }
+      }
+
 		}
 		
 		"react" in {
+      object Reactor {
+        val processor = actor {
+          loop {
+            receive {
+              case Message(index) =>
+                reply(Reply(index + 2))
+            }
+          }
+        }
+      }
+
+      statTiming("blocking_react", 50) {
+        var index = 0
+        while(index < 1000) {
+          Reactor.processor !? Message(index)
+          index += 1
+        }
+      }
 		}
 	}
 }
