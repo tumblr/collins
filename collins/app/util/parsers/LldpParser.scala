1743a4dc28a951ca60c5a2e11e70bf6f423c6901
Blake Matheny <bmatheny@mobocracy.net>
2011-12-28 12:04:04 -0500

    Finishing adding parsers stuff. Damn it, should have done git mv

---
+++ collins/app/util/parsers/LldpParser.scala
@@ -0,0 +1,8 @@
+package util
+package parsers
+
+import play.api._
+import scala.xml.{Elem, MalformedAttributeException, Node, NodeSeq, XML}
+
+class LldpParser(txt: String, config: Map[String,String] = Map.empty) {
+}
