1b5ef64c12c2cc8be5a70604262e7b4cd4691371
Blake Matheny <bmatheny@mobocracy.net>
2012-02-13 17:29:46 -0500

    Allow query for assets missing a particular attribute.

--- collins/app/models/MetaWrapper.scala
+++ collins/app/models/MetaWrapper.scala
@@ -88,43 +88,83 @@ object MetaWrapper {
   }
 
   def findAssetsByMeta(page: PageParams, params: Seq[Tuple2[AssetMeta, String]], afinder: AssetFinder): Page[Asset] = {
-    val simpleSql = collectParams(params)
-    val queryPlaceholder = """
-      select %s from asset_meta_value amv
-      where %s
-    """
-    val finderQuery = afinder.asQueryFragment()
-    val finderQueryFrag = finderQuery.sql.query match {
+    val assetQuery = afinder.asQueryFragment()
+    val assetQueryFragment = assetQuery.sql.query match {
       case empty if empty.isEmpty => ""
       case nonEmpty => nonEmpty + " and "
     }
-    val allFragmentsString = simpleSql.sql.query
-    val query = queryPlaceholder.format("distinct asset_id", allFragmentsString)
-    val countQuery = queryPlaceholder.format("count(distinct asset_id)", allFragmentsString)
-    val paramsNoPaging = finderQuery.params ++ simpleSql.params
-    val paramsWithPaging = Seq(
+
+    val metaQuery = collectParams(params)
+
+    val subQuery = """
+      select %s from asset_meta_value amv
+      where %s""".format("distinct asset_id", metaQuery.sql.query)
+
+    val paramsWithoutPaging = assetQuery.params ++ metaQuery.params
+    val allQueryParams = Seq(
       ('pageSize -> toParameterValue(page.size)),
       ('offset -> toParameterValue(page.offset))
-    ) ++ paramsNoPaging
+    ) ++ paramsWithoutPaging
+
     Model.withConnection { implicit con =>
-      val assets = Asset.find("%s id in (%s) limit {pageSize} offset {offset}".format(finderQueryFrag, query)).on(paramsWithPaging:_*).list()
-      val count = Asset.count("%s id in (%s)".format(finderQueryFrag, query)).on(paramsNoPaging:_*).as(scalar[Long])
+      val assets = Asset.find(
+        "%s id in (%s) limit {pageSize} offset {offset}".format(
+          assetQueryFragment,
+          subQuery
+        )
+      ).on(allQueryParams:_*).list()
+      val count = Asset.count(
+        "%s id in (%s)".format(
+          assetQueryFragment,
+          subQuery
+        )
+      ).on(paramsWithoutPaging:_*).as(scalar[Long])
       Page(assets, page.page, page.offset, count)
     }
   }
 
- 
+  sealed trait QueryType {
+    val sql: SimpleSql[Row]
+  }
+  case class ExcludeQuery(sql: SimpleSql[Row]) extends QueryType
+  case class IncludeQuery(sql: SimpleSql[Row]) extends QueryType
+  // This generates a subquery that looks roughly like
+  //  ((value1 LIKE thing) OR (value2 LIKE other)...) AND ((value3 NOT EXISTS) AND (value4 NOT EXISTS))
+  //  This creates a query that finds assets that match specific values, and is missing certain
+  //  other attributes. This is not straight forward code.
   private[this] def collectParams(assetMeta: Seq[Tuple2[AssetMeta, String]]): SimpleSql[Row] = {
-    val result: Seq[SimpleSql[Row]] = assetMeta.zipWithIndex.map { case(tuple, size) =>
-      val metaName = "asset_meta_id_%d".format(size)
-      val metaValueName = "asset_meta_value_value_%d".format(size)
-      val ivalue = tuple._2
-      val nvalue = regexWrap(ivalue)
-      SqlQuery("(amv.asset_meta_id={%s} and amv.value REGEXP {%s})".format(
-        metaName, metaValueName
-      )).on(metaName -> tuple._1.getId, metaValueName -> nvalue)
+    val result: Seq[QueryType] = assetMeta.zipWithIndex.map { case(tuple, size) =>
+      val metaName = "asset_meta_id_%d".format(size) // Name for query expansion
+      val metaValueName = "asset_meta_value_value_%d".format(size) // value for query expansion
+      val metaId = tuple._1.getId // meta id of thing we're querying for
+      val initValue = tuple._2 // initial value that we're looking for, associated with metaId
+      if (initValue.isEmpty) { // if it's empty, generate a sub-query that excludes these assets
+        val filterQuery = """
+        (select CASE count(*) WHEN 0 THEN 1 ELSE 0 END from asset_meta_value amv2 where amv2.asset_id = amv.asset_id
+        and amv2.asset_meta_id={%s})
+        """.format(metaName) // if count is positive 0 (exclude this asset), otherwise 1.
+        ExcludeQuery(SqlQuery(filterQuery).on(metaName -> metaId))
+      } else {
+        val regexValue = regexWrap(initValue) // regex to use for searching
+        val includeQuery = "(amv.asset_meta_id={%s} AND amv.value REGEXP {%s})".format(
+          metaName, metaValueName
+        )
+        val simpleQuery = SqlQuery(includeQuery).on(metaName -> metaId, metaValueName -> regexValue)
+        IncludeQuery(simpleQuery)
+      }
     }
-    DaoSupport.flattenSql(result, " or ")
+    val includes = result.filter(_.isInstanceOf[IncludeQuery]).map(_.sql)
+    val excludes = result.filter(_.isInstanceOf[ExcludeQuery]).map(_.sql)
+    // the and/or stuff should be configurable but it's not
+    val includeSql = includes match {
+      case Nil => None
+      case rows => Some(DaoSupport.flattenSql(rows, " or "))
+    }
+    val excludeSql = excludes match {
+      case Nil => None
+      case rows => Some(DaoSupport.flattenSql(rows, " and "))
+    }
+    DaoSupport.flattenSql(Seq(includeSql, excludeSql).filter(_.isDefined).map(_.get), " and ")
   }
 
   private[this] def regexWrap(s: String): String = {
