72a512a1734e01157a07b8da7e2023d6fdce2ed2
Blake Matheny <bmatheny@mobocracy.net>
2012-02-20 20:04:35 -0500

    Support for reading profiles from provisioner yaml

--- collins/project/Build.scala
+++ collins/project/Build.scala
@@ -13,7 +13,8 @@ object ApplicationBuild extends Build {
       "org.bouncycastle" % "bcprov-jdk16" % "1.46",
       "com.twitter" %% "util-core" % "1.12.12",
       "com.twitter" %% "finagle-http" % "1.10.0",
-      "org.jsoup" % "jsoup" % "1.6.1"
+      "org.jsoup" % "jsoup" % "1.6.1",
+      "org.yaml" % "snakeyaml" % "1.10"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
