5b3469fc556b1c392dcd36ff5cba073230939a8c
Blake Matheny <bmatheny@mobocracy.net>
2012-02-23 10:11:01 -0500

    Use tattler interface for logging.

--- collins/app/util/Tattler.scala
+++ collins/app/util/Tattler.scala
@@ -1,14 +1,28 @@
 package util
 
 import models._
+import java.sql.Connection
 
-sealed abstract class Tattler(val source: AssetLog.Sources) {
+sealed abstract class Tattler(val source: AssetLog.Sources, val pString: Option[String] = None) {
   protected def message(user: Option[User], msg: String) = {
-    "User %s: %s".format(user.map(_.username).getOrElse("unknown"), msg)
+    pString.map(s => s + ": " + msg).getOrElse {
+      "User %s: %s".format(user.map(_.username).getOrElse("unknown"), msg)
+    }
   }
-  def warning(asset: Asset, user: Option[User], msg: String) = {
+  def warning(asset: Asset, user: Option[User], msg: String): AssetLog = {
+    Model.withConnection { con =>
+      warning(asset, user, msg, con)
+    }
+  }
+  def warning(asset: Asset, user: Option[User], msg: String, con: Connection): AssetLog = {
+    implicit val c: Connection = con
+    AssetLog.warning(
+      asset, message(user, msg), AssetLog.Formats.PlainText, source
+    ).create()
+  }
+  def notice(asset: Asset, user: Option[User], msg: String) = {
     Model.withConnection { implicit con =>
-      AssetLog.warning(
+      AssetLog.notice(
         asset, message(user, msg), AssetLog.Formats.PlainText, source
       ).create()
     }
@@ -20,13 +34,18 @@ sealed abstract class Tattler(val source: AssetLog.Sources) {
       ).create()
     }
   }
-  def notice(asset: Asset, user: Option[User], msg: String) = {
-    Model.withConnection { implicit con =>
-      AssetLog.note(
-        asset, message(user, msg), AssetLog.Formats.PlainText, source
-      ).create()
+  def informational(asset: Asset, user: Option[User], msg: String): AssetLog = {
+    Model.withConnection { con =>
+      informational(asset, user, msg, con)
     }
+  }
+  def informational(asset: Asset, user: Option[User], msg: String, con: Connection): AssetLog = {
+    implicit val c: Connection = con
+    AssetLog.informational(
+      asset, message(user, msg), AssetLog.Formats.PlainText, source
+    ).create()
   }
 }
 object UserTattler extends Tattler(AssetLog.Sources.User)
 object ApiTattler extends Tattler(AssetLog.Sources.Api)
+object InternalTattler extends Tattler(AssetLog.Sources.Internal, Some("Internal"))
