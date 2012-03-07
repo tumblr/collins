a199b7aa64892194868e51e2a6334582d3d5010e
Petar Maymounkov <petar@tumblr.com>
2012-03-07 10:56:09 -0500

    ok

--- cmd/p_reposync/main.go
+++ cmd/p_reposync/main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"strings"
 )
+
+// XXX: Check that the repository is tumblr and not something else
 
 var (
 	flagRsync       *string = flag.String("rsync", envOrDefault("rsync", "/use/bin/rsync"), "path to rsync executable")
 	flagRemoteHost  *string = flag.String("host", "tumblr", "hostname of remote")
 	flagRemoteSuper *string = flag.String("super", "/var/www/apps", "parent of repo directory on remote")
+	flagRepos       *string = flag.String("repos", "tumblr", "'"+string(filepath.ListSeparator) + "'-separated list of repo names to sync")
 )
 
 func envOrDefault(cmd, dfl string) string {
@@ -50,14 +55,19 @@ func main() {
 		log.Fatal("Target is not absolute")
 	}
 	local := path.Clean(a)
+	repos := filepath.SplitList(*flagRepos)
+	if len(repos) == 0 {
+		log.Fatal("List of repos empty")
+	}
 
 	if !path.IsAbs(*flagRemoteSuper) {
 		log.Fatal("Remote repo super-directory is not absolute")
 	}
 	p_ := strings.Split(local, "/")
-	for i := 0; i < len(p_); i++ {
+	for i := 1; i < len(p_); i++ {
+		repo := p_[len(p_)-i-1]
 		localRepo := "/" + path.Join(p_[:len(p_)-i]...)
-		if IsGitRepoNoErr(localRepo) {
+		if InStringSlice(repo, repos) && IsGitRepoNoErr(localRepo) {
 			subPath := path.Join(p_[len(p_)-i-1:]...)
 			remoteSuper := path.Clean(*flagRemoteSuper) + "/"
 			remote := path.Join(remoteSuper, subPath)
@@ -69,12 +79,23 @@ func main() {
 				)
 			if err != nil {
 				log.Printf("=== out ===\n%s\n=== err ===\n%s\n========\n", rsyncStdout, rsyncStderr)
-				log.Fatalf("Error rsync-ing (%s)", err)
+				log.Fatalf("rsync error (%s)", err)
 			}
+			fmt.Printf("rsync OK: %s ——> %s:%s\n", local, *flagRemoteHost, remote)
 			os.Exit(0)
 		}
 	}
-	log.Fatal("Target is not inside a repo")
+	// If target is not inside a repo, don't complain
+	os.Exit(0)
+}
+
+func InStringSlice(s string, list []string) bool {
+	for _, x := range list {
+		if s == x {
+			return true
+		}
+	}
+	return false
 }
 
 // TODO: This should be moved to a pkg
