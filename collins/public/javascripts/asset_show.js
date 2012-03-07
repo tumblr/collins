9816cbbaf3df0eeb518a47b5ebb13de006884e0c
Blake Matheny <bmatheny@mobocracy.net>
2012-03-07 13:27:27 -0500

    Allow custom pools and secondary roles.

--- collins/public/javascripts/asset_show.js
+++ collins/public/javascripts/asset_show.js
@@ -15,25 +15,52 @@ $(document).ready(function() {
   function showContainer(id) {
     $(id).show();
   };
-  function setProvisionerValue(id, profile, values, name_id, label) {
+  function setProvisionerValue(id, profile, values, name_id, label, type) {
     var config_value = profile[name_id];
     if (config_value) {
       $(id).find('div.input').append('<span id="'+name_id+'" class="uneditable-input">' + config_value + "</span>");
     } else {
-      var html = '<select name="'+name_id+'" id="'+name_id+'"><option value="" selected="selected"></option>';
+      var input = $(id).find('div.input')
+      input.empty();
+      if ($(id).hasClass('haveChoice')) {
+        var check_name = name_id + '_check';
+        var check = '';
+        if (type == 'text') {
+          check = '<input type="checkbox" checked name="'+check_name+'" value="text"> Custom ' + label;
+        } else {
+          check = '<input type="checkbox" name="'+check_name+'" value="text"> Custom ' + label;
+        }
+        check = '<div>' + check + '</div>';
+        input.append(check);
+        $(id).find('input[type=checkbox][name='+check_name+']').each(function() {
+          $(this).change(function() {
+            if ($(this).is(':checked')) {
+              setProvisionerValue(id, profile, values, name_id, label, 'text');
+            } else {
+              setProvisionerValue(id, profile, values, name_id, label, 'list');
+            }
+          });
+        });
+      }
       var help = '';
-      values.forEach(function(v) {
-        html += '<option value="'+v+'">'+v+'</option>'
-      });
       if (profile['requires_' + name_id]) {
         help = label + ' is required';
       } else {
         help = label + ' is optional';
       }
       help = '<span class="help-block">' + help + '</span>';
-      var input = $(id).find('div.input')
-      input.append(html)
-      input.append(help);
+      if (type == 'text') {
+        var html = '<input type="text" name="'+name_id+'" id="'+name_id+'">';
+        input.append(html);
+        input.append(help);
+      } else {
+        var html = '<select name="'+name_id+'" id="'+name_id+'"><option value="" selected="selected"></option>';
+        values.forEach(function(v) {
+          html += '<option value="'+v+'">'+v+'</option>'
+        });
+        input.append(html)
+        input.append(help);
+      }
     }
   };
   function isEmpty(v) {
@@ -58,9 +85,9 @@ $(document).ready(function() {
         hideContainer(v);
       });
     } else {
-      setProvisionerValue(primary_role, profile, config["primary_roles"], 'primary_role', 'Primary Role');
-      setProvisionerValue(pool, profile, config["pools"], 'pool', 'Pool');
-      setProvisionerValue(secondary_role, profile, config["secondary_roles"], 'secondary_role', 'Secondary Role');
+      setProvisionerValue(primary_role, profile, config["primary_roles"], 'primary_role', 'Primary Role', 'List');
+      setProvisionerValue(pool, profile, config["pools"], 'pool', 'Pool', 'List');
+      setProvisionerValue(secondary_role, profile, config["secondary_roles"], 'secondary_role', 'Secondary Role', 'List');
       containers.forEach(function(v) {
         showContainer(v);
       });
