8d69f3dad33e43d27912f2aacc96f79a067ba89a
Blake Matheny <bmatheny@mobocracy.net>
2012-03-08 18:16:41 -0500

    Added activation support.

---
+++ collins/app/controllers/actors/ActivationProcessor.scala
@@ -0,0 +1,17 @@
+package controllers
+package actors
+
+import akka.util.Duration
+import play.api.mvc.{AnyContent, Request}
+import util.SoftLayer
+
+case class ActivationProcessor(slId: Long, userTimeout: Option[Duration] = None)(implicit req: Request[AnyContent]) extends BackgroundProcess[Boolean] {
+  override def defaultTimeout: Duration = Duration.parse("60 seconds")
+  val timeout = userTimeout.getOrElse(defaultTimeout)
+
+  def run(): Boolean = {
+    val plugin = SoftLayer.pluginEnabled.get
+    plugin.activateServer(slId)()
+  }
+}
+
