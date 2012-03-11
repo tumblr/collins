c43c3601ba482142e6216a3625937cc99cd7d1f6
Blake Matheny <bmatheny@mobocracy.net>
2012-03-11 05:03:17 -0400

    Refactored AssetLog to use squeryl

--- collins/app/models/AssetLog.scala
+++ collins/app/models/AssetLog.scala
@@ -1,27 +1,58 @@
 package models
 
-import Model.defaults._
 import conversions._
 import util.Helpers
 
-import anorm._
-import anorm.SqlParser._
 import play.api.libs.json._
+
+import org.squeryl.PrimitiveTypeMode._
+import org.squeryl.{Schema, Table}
 
 import java.sql.{Connection, Timestamp}
 import java.util.Date
 
-// Index on (asset_id, message_type)
+object LogFormat extends Enumeration {
+  type LogFormat = LogFormat.Value
+  val PlainText = Value(0, "text/plain")
+  val Json = Value(1, "application/json")
+}
+object LogMessageType extends Enumeration {
+  type LogMessageType = Value
+  val Emergency = Value(0, "EMERGENCY")
+  val Alert = Value(1, "ALERT")
+  val Critical = Value(2, "CRITICAL")
+  val Error = Value(3, "ERROR")
+  val Warning = Value(4, "WARNING")
+  val Notice = Value(5, "NOTICE")
+  val Informational = Value(6, "INFORMATIONAL")
+  val Debug = Value(7, "DEBUG")
+  val Note = Value(8, "NOTE")
+}
+object LogSource extends Enumeration {
+  type LogSource = Value
+  val Internal = Value(0, "INTERNAL")
+  val Api = Value(1, "API")
+  val User = Value(2, "USER")
+}
+import LogFormat.LogFormat
+import LogMessageType.LogMessageType
+import LogSource.LogSource
+
 case class AssetLog(
-  id: Pk[java.lang.Long],
-  asset_id: Id[java.lang.Long],
+  asset_id: Long,
   created: Timestamp,
-  format: java.lang.Byte,
-  source: java.lang.Byte,
-  message_type: java.lang.Byte,
-  message: String)
+  format: LogFormat = LogFormat.PlainText,
+  source: LogSource = LogSource.Internal,
+  message_type: LogMessageType = LogMessageType.Emergency,
+  message: String,
+  id: Long = 0) extends ValidatedEntity[Long]
 {
-  require(message != null && message.length > 0)
+  def this() = // 0 arg constructor since we have enums
+    this(0,new Date().asTimestamp, LogFormat.PlainText, LogSource.Internal, LogMessageType.Emergency, "", 0)
+
+  override def validate() {
+    require(message != null && message.length > 0)
+  }
 
   def forJsonObject(): Seq[(String,JsValue)] = Seq(
     "ID" -> JsNumber(getId()),
@@ -36,35 +67,35 @@ case class AssetLog(
     })
   )
 
-  def getFormat(): AssetLog.Formats = AssetLog.Formats(format.intValue)
-  def isJson(): Boolean = getFormat() == AssetLog.Formats.Json
-  def isPlainText(): Boolean = getFormat() == AssetLog.Formats.PlainText
+  def getFormat(): LogFormat = format
+  def isJson(): Boolean = getFormat() == LogFormat.Json
+  def isPlainText(): Boolean = getFormat() == LogFormat.PlainText
 
-  def getSource(): AssetLog.Sources = AssetLog.Sources(source.intValue)
-  def isInternalSource(): Boolean = getSource() == AssetLog.Sources.Internal
-  def isApiSource(): Boolean = getSource() == AssetLog.Sources.Api
-  def isUserSource(): Boolean = getSource() == AssetLog.Sources.User
+  def getSource(): LogSource = source
+  def isInternalSource(): Boolean = getSource() == LogSource.Internal
+  def isApiSource(): Boolean = getSource() == LogSource.Api
+  def isUserSource(): Boolean = getSource() == LogSource.User
 
-  def getMessageType(): AssetLog.MessageTypes = AssetLog.MessageTypes(message_type.intValue)
+  def getMessageType(): LogMessageType = message_type
   def isEmergency(): Boolean =
-    getMessageType() == AssetLog.MessageTypes.Emergency
+    getMessageType() == LogMessageType.Emergency
   def isAlert(): Boolean =
-    getMessageType() == AssetLog.MessageTypes.Alert
+    getMessageType() == LogMessageType.Alert
   def isCritical(): Boolean =
-    getMessageType() == AssetLog.MessageTypes.Critical
+    getMessageType() == LogMessageType.Critical
   def isError(): Boolean =
-    getMessageType() == AssetLog.MessageTypes.Error
+    getMessageType() == LogMessageType.Error
   def isWarning(): Boolean =
-    getMessageType() == AssetLog.MessageTypes.Warning
+    getMessageType() == LogMessageType.Warning
   def isNotice(): Boolean =
-    getMessageType() == AssetLog.MessageTypes.Notice
+    getMessageType() == LogMessageType.Notice
   def isInformational(): Boolean =
-    getMessageType() == AssetLog.MessageTypes.Informational
+    getMessageType() == LogMessageType.Informational
   def isDebug(): Boolean =
-    getMessageType() == AssetLog.MessageTypes.Debug
+    getMessageType() == LogMessageType.Debug
 
-  def getId(): Long = id.get
-  def getAssetId(): Long = asset_id.get
+  def getId(): Long = id
+  def getAssetId(): Long = asset_id
   def getAsset(): Asset = Asset.findById(getAssetId()).get
 
   def withException(ex: Throwable) = {
@@ -95,7 +126,7 @@ case class AssetLog(
   }
 
   def create()(implicit con: Connection) = {
-    if (id == NotAssigned) {
+    if (id == 0) {
       AssetLog.create(this)
     } else {
       throw new IllegalStateException("Can't create log entry, already created")
@@ -104,134 +135,122 @@ case class AssetLog(
 
 }
 
-object AssetLog extends Magic[AssetLog](Some("asset_log")) {
-  def apply(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources, mt: AssetLog.MessageTypes) = {
-    new AssetLog(NotAssigned, Id(asset.getId), new Date().asTimestamp,
-      format.id.toByte, source.id.toByte,
-      mt.id.toByte,
-      message)
+object AssetLog extends Schema with AnormAdapter[AssetLog] {
+
+  val assetLog = table[AssetLog]("asset_log")
+  on(assetLog)(a => declare(
+    a.id is(autoIncremented,primaryKey),
+    columns(a.asset_id, a.message_type) are(indexed)
+  ))
+
+  override def table = assetLog
+  override def delete(t: AssetLog): Int = 0
+
+  def apply(asset: Asset, message: String, format: LogFormat, source: LogSource, mt: LogMessageType) = {
+    new AssetLog(asset.getId, new Date().asTimestamp, format, source, mt, message)
   }
 
   // A "panic" condition - notify all tech staff on call? (earthquake? tornado?) - affects multiple
   // apps/servers/sites...
-  def emergency(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Emergency)
+  def emergency(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Emergency)
   }
 
   // Should be corrected immediately, but indicates failure in a primary system - fix CRITICAL
   // problems before ALERT - example is loss of primary ISP connection
-  def critical(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Critical)
+  def critical(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Critical)
   }
 
   // Should be corrected immediately - notify staff who can fix the problem - example is loss of
   // backup ISP connection
-  def alert(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Alert)
+  def alert(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Alert)
   }
 
   // Non-urgent failures - these should be relayed to developers or admins; each item must be
   // resolved within a given time
-  def error(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Error)
+  def error(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Error)
   }
 
   // Warning messages - not an error, but indication that an error will occur if action is not
   // taken, e.g. file system 85% full - each item must be resolved within a given time
-  def warning(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Warning)
+  def warning(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Warning)
   }
 
   // Events that are unusual but not error conditions - might be summarized in an email to
   // developers or admins to spot potential problems - no immediate action required
-  def notice(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Notice)
+  def notice(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Notice)
   }
 
-  def note(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Note)
+  def note(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Note)
   }
 
   // Normal operational messages - may be harvested for reporting, measuring throughput, etc - no
   // action required
-  def informational(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Informational)
+  def informational(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Informational)
   }
 
   //Info useful to developers for debugging the application, not useful during operations
-  def debug(asset: Asset, message: String, format: AssetLog.Formats, source: AssetLog.Sources) = {
-      apply(asset, message, format, source, AssetLog.MessageTypes.Debug)
+  def debug(asset: Asset, message: String, format: LogFormat, source: LogSource) = {
+      apply(asset, message, format, source, LogMessageType.Debug)
   }
 
   def list(asset: Option[Asset], page: Int = 0, pageSize: Int = 10, sort: String = "DESC", filter: String = ""): Page[AssetLog] = {
-    val orderBy: String = sort.toUpperCase match {
-      case "ASC" => "ORDER BY ID ASC"
-      case _ => "ORDER BY ID DESC"
-    }
-    val messageFilter = filter.isEmpty match {
-      case true => ""
-      case false => try {
-        val (id, equalityCheck) = if (filter.startsWith("!")) {
-          (AssetLog.MessageTypes.withName(filter.drop(1).toUpperCase).id, "!=")
-        } else {
-          (AssetLog.MessageTypes.withName(filter.toUpperCase).id, "=")
-        }
-        "and message_type %s %d".format(equalityCheck, id)
-      } catch {
-        case _ => ""
+    val negate = filter.startsWith("!")
+    val messageType = try {
+      if (negate) {
+        Some(LogMessageType.withName(filter.drop(1).toUpperCase))
+      } else {
+        Some(LogMessageType.withName(filter.toUpperCase))
       }
+    } catch {
+      case e => None
     }
+    val uSort = sort.toUpperCase match {
+      case "ASC" => "ASC"
+      case _ => "DESC"
+    }
+    _list(asset.map(_.getId), page, pageSize, uSort, messageType, negate)
+  }
+
+  private def _list(asset_id: Option[Long], page: Int, pageSize: Int, sort: String, filter: Option[LogMessageType], negate: Boolean = false): Page[AssetLog] = {
     val offset = pageSize * page
-    // Can't use on() for asc/desc because we don't want it quoted
-    Model.withConnection { implicit con =>
-      asset match {
-        case Some(asset) =>
-          val query = "asset_id={asset_id} %s %s limit {pageSize} offset {offset}".format(messageFilter, orderBy)
-          val rows = AssetLog.find(query).on(
-            'asset_id -> asset.getId,
-            'pageSize -> pageSize,
-            'offset -> offset
-          ).list()
-          val totalRows = AssetLog.count("asset_id={asset_id} %s".format(messageFilter)).on(
-            'asset_id -> asset.getId
-          ).as(scalar[Long])
-          Page(rows, page, offset, totalRows)
-        case None =>
-          val query = "where 1=1 %s %s limit {pageSize} offset {offset}".format(messageFilter, orderBy)
-          val rows = AssetLog.find(query).on(
-            'pageSize -> pageSize,
-            'offset -> offset
-          ).list()
-          val totalRows = AssetLog.count("1=1 %s".format(messageFilter)).as(scalar[Long])
-          Page(rows, page, offset, totalRows)
-      }
+    withConnection {
+      val results = from(assetLog)(a =>
+        where(whereClause(a, asset_id, filter, negate))
+        select(a)
+        orderBy {
+          if (sort == "ASC") a.id asc
+          else a.id desc
+        }
+      ).page(page, pageSize).toList
+      val totalCount = from(assetLog)(a =>
+        where(whereClause(a, asset_id, filter, negate))
+        compute(count)
+      )
+      Page(results, page, offset, totalCount)
     }
   }
 
-  type Formats = Formats.Value
-  object Formats extends Enumeration(0) {
-    val PlainText = Value(0, "text/plain")
-    val Json = Value(1, "application/json")
-  }
-
-  type Sources = Sources.Value
-  object Sources extends Enumeration(0) {
-    val Internal = Value(0, "INTERNAL")
-    val Api = Value(1, "API")
-    val User = Value(2, "USER")
-  }
-
-  type MessageTypes = MessageTypes.Value
-  object MessageTypes extends Enumeration(0) {
-    val Emergency = Value(0, "EMERGENCY")
-    val Alert = Value(1, "ALERT")
-    val Critical = Value(2, "CRITICAL")
-    val Error = Value(3, "ERROR")
-    val Warning = Value(4, "WARNING")
-    val Notice = Value(5, "NOTICE")
-    val Informational = Value(6, "INFORMATIONAL")
-    val Debug = Value(7, "DEBUG")
-    val Note = Value(8, "NOTE")
+  private def whereClause(a: AssetLog, asset_id: Option[Long], filter: Option[LogMessageType], negate: Boolean) = {
+    filter match {
+      case None =>
+        (a.asset_id === asset_id.?)
+      case Some(f) =>
+        (a.asset_id === asset_id.?) and {
+          if (negate) {
+            (a.message_type <> filter.get)
+          } else {
+            (a.message_type === filter.get)
+          }
+        }
+    }
   }
 
 }
