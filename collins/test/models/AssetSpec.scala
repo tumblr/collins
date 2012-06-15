8acf27bf1a00463b60798ac91e7de5a8ea53fa5c
Dan Simon <dan@tumblr.com>
2012-06-15 15:40:37 -0400

    adding nodeclass test, fixing nodeclass bugs set comparison bugs

--- collins/test/models/AssetSpec.scala
+++ collins/test/models/AssetSpec.scala
@@ -38,6 +38,22 @@ class AssetSpec extends ApplicationSpecification {
       }
     }
 
+    "Support nodeclass" in {
+      "nodeClass" in new mocknodeclass {
+        def createAssetMetas(asset: Asset, metamap: Map[String, String]) = metamap
+          .map{ case (k,v) => 
+            AssetMetaValue.create(AssetMetaValue(asset.id, AssetMeta.findOrCreateFromName(k).id, 0, v))
+          }
+        val nodeclass = Asset.create(Asset(nodeclassTag, nodeclassStatus,nodeclassType))
+        val testAsset = Asset.create(Asset(assetTag, assetStatus, assetType))
+        val nodeclassMetas = createAssetMetas(nodeclass, (nodeclassMetaTags + nodeclassIdentifierTag))
+        val assetMetas = createAssetMetas(testAsset, nodeclassMetaTags)
+        testAsset.nodeClass must_== Some(nodeclass)
+      }
+
+    }
+        
+
     "Support getters/finders" in {
 
       "findByTag" in new concreteasset {
@@ -51,6 +67,8 @@ class AssetSpec extends ApplicationSpecification {
         assets.total must beGreaterThan(0L)
         assets.items must have {_.tag == assetTag}
       }
+
+      
 
       "getAllAttributes" in new concreteasset {
         val maybeAsset = Asset.findByTag(assetTag)
@@ -78,6 +96,17 @@ class AssetSpec extends ApplicationSpecification {
     val assetStatus = Status.Enum.Incomplete
     val assetType = AssetType.Enum.ServerNode
     val assetId = 1
+  }
+
+  trait mocknodeclass extends Scope {
+    val nodeclassTag = "test_nodeclass"
+    val nodeclassStatus = Status.Enum.Allocated
+    val nodeclassType = AssetType.Enum.Config
+    val nodeclassIdentifierTag = ("IS_NODECLASS" -> "true")
+    val nodeclassMetaTags = Map("FOO" -> "BAR", "BAZ" -> "BAAAAZ")
+    val assetTag = "nodeclasstest"
+    val assetStatus = Status.Enum.Allocated
+    val assetType = AssetType.Enum.ServerNode
   }
 
 
