adb205c8add335d2186a2645da187167b91fa586
Blake Matheny <bmatheny@mobocracy.net>
2012-06-15 21:06:09 -0400

    Client provisioning support
        collins-shell provision list
        collins-shell provision host
    
    Also some minor cleanup of unused methods.

--- collins/app/controllers/ApiResponse.scala
+++ collins/app/controllers/ApiResponse.scala
@@ -18,40 +18,20 @@ object ApiResponse extends ApiResponse {
   def formatJsonMessage(status: Results.Status, data: JsObject): JsObject = {
     formatJsonMessage(statusToString(status), data)
   }
-  def formatJsonMessage[V](status: Results.Status, data: Seq[(String,V)])(implicit m: Manifest[V]): JsObject = {
-    formatJsonMessage(statusToString(status), data)
-  }
-  def formatJsonMessage[V](status: String, data: Seq[(String,V)])(implicit m: Manifest[V]): JsObject = {
-    val jso = m match {
-      case s if s == Manifest.classType(classOf[String]) =>
-        JsObject(data.map(kv => kv._1 -> JsString(kv._2.asInstanceOf[String])))
-      case j if j == Manifest.classType(classOf[JsValue]) =>
-        JsObject(data.asInstanceOf[Seq[(String,JsValue)]])
-      case _ =>
-        throw new RuntimeException("Unhandled conversion for Map with values of type " + m.toString)
-    }
-    formatJsonMessage(status, jso)
-  }
 
   def isJsonErrorMessage(js: JsValue): Boolean = {
     (js \ "status").asOpt[String].map(e => e.contains("error")).getOrElse(false)
   }
-  def getJsonErrorMessage(js: JsValue, default: String) = {
-    val isError = (js \ "status").asOpt[String].map(e => e.contains("error")).getOrElse(false)
-    if (isError) {
-      (js \ "data" \ "message").asOpt[String].getOrElse(default)
-    } else {
-      default
-    }
-  }
 
   def formatJsonError(msg: String, ex: Option[Throwable]): JsObject = {
-    val seq = Seq("message" -> JsString(msg))
-    val exSeq = ex.map { e =>
-      Seq("details" -> JsObject(formatException(e).map(kv => kv._1 -> JsString(kv._2))))
-    }.getOrElse(Seq())
-    val dataSeq = seq ++ exSeq
-    formatJsonMessage("error", JsObject(dataSeq))
+    val message = Seq("message" -> JsString(msg))
+    val optional = ex match {
+      case None => Nil
+      case Some(e) =>
+        val seq = formatException(e).map(s => s._1 -> JsString(s._2))
+        Seq("details" -> JsObject(seq))
+    }
+    formatJsonMessage("error", JsObject(message ++ optional))
   }
 
   def bashError(msg: String, status: Results.Status = Results.BadRequest, ex: Option[Throwable]) = {
@@ -69,27 +49,27 @@ DATA_MESSAGE='%s';
 
   def statusToString(status: Results.Status): String = {
     status.header.status match {
-      case 200 => "successful -> ok"
-      case 201 => "successful -> created"
-      case 202 => "successful -> accepted"
-      case ok if ok >= 200 && ok < 300 => "successful -> other"
-      case 400 => "client error -> bad request"
-      case 401 => "client error -> unauthorized"
-      case 403 => "client error -> forbidden"
-      case 404 => "client error -> not found"
-      case 405 => "client error -> method not allowed"
-      case 406 => "client error -> not acceptable"
-      case 409 => "client error -> conflict"
-      case userErr if userErr >= 400 && userErr < 500 => "client error -> unknown"
-      case 500 => "server error -> internal server error"
-      case 501 => "server error -> not implemented"
-      case srvErr if srvErr >= 500 => "server error -> unknown"
-      case n => "unknown -> %d".format(n)
+      case 200 => "success:ok"
+      case 201 => "success:created"
+      case 202 => "success:accepted"
+      case ok if ok >= 200 && ok < 300 => "success:other"
+      case 400 => "client_error:bad request"
+      case 401 => "client_error:unauthorized"
+      case 403 => "client_error:forbidden"
+      case 404 => "client_error:not found"
+      case 405 => "client_error:method not allowed"
+      case 406 => "client_error:not acceptable"
+      case 409 => "client_error:conflict"
+      case userErr if userErr >= 400 && userErr < 500 => "client_error:unknown"
+      case 500 => "server_error:internal server error"
+      case 501 => "server_error:not implemented"
+      case srvErr if srvErr >= 500 => "server_error:unknown"
+      case n => "unknown:%d".format(n)
     }
   }
 
   def jsonError(msg: String, status: Results.Status = Results.BadRequest, ex: Option[Throwable]) = {
-    val output: JsValue = formatJsonError(msg, ex)
+    val output: JsValue = formatJsonMessage(status, formatJsonError(msg, ex))
     status(output).as(contentTypeWithCharset(JsonOutput()))
   }
 
---
+++ collins/app/controllers/actions/asset/GetProvisioningProfilesAction.scala
@@ -0,0 +1,49 @@
+package controllers
+package actions
+package asset
+
+import util.{Provisioner, SecuritySpecification}
+import com.tumblr.play.ProvisionerProfile
+import play.api.libs.json._
+
+case class GetProvisioningProfilesAction(
+  spec: SecuritySpecification,
+  handler: SecureController
+) extends SecureAction(spec, handler) with AssetAction {
+
+  case class ActionDataHolder(profiles: Set[ProvisionerProfile]) extends RequestDataHolder
+
+  override def validate(): Validation = Provisioner.plugin match {
+    case None =>
+      Left(RequestDataHolder.error500("Provisioner plugin not enabled"))
+    case Some(plugin) =>
+      Right(ActionDataHolder(plugin.profiles))
+  }
+
+  override def execute(rd: RequestDataHolder) = rd match {
+    case ActionDataHolder(profiles) =>
+      val obj = JsObject(Seq("PROFILES" -> formatProfiles(profiles)))
+      ResponseData(Status.Ok, obj)
+  }
+
+  protected def formatProfiles(profiles: Set[ProvisionerProfile]) = JsArray(
+    profiles.toList.map { profile =>
+      val role = profile.role
+      val seq = Seq(
+        "PROFILE"                 -> JsString(profile.identifier),
+        "LABEL"                   -> JsString(profile.label),
+        "PREFIX"                  -> JsString(profile.prefix),
+        "SUFFIX_ALLOWED"          -> JsBoolean(profile.allow_suffix),
+        "PRIMARY_ROLE"            -> stringOrNull(role.primary_role),
+        "REQUIRES_PRIMARY_ROLE"   -> JsBoolean(role.requires_primary_role),
+        "POOL"                    -> stringOrNull(role.pool),
+        "REQUIRES_POOL"           -> JsBoolean(role.requires_pool),
+        "SECONDARY_ROLE"          -> stringOrNull(role.secondary_role),
+        "REQUIRES_SECONDARY_ROLE" -> JsBoolean(role.requires_secondary_role)
+      )
+      JsObject(seq)
+    }
+  )
+
+  private def stringOrNull(s: Option[String]): JsValue = s.map(JsString(_)).getOrElse(JsNull)
+}
