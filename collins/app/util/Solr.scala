d5cd7f2b184d6dde1c78036a840f1c0e2c8fc6d4
Dan Simon <dan@tumblr.com>
2012-07-11 18:14:51 -0400

    basic Solr serialization

--- collins/app/util/Solr.scala
+++ collins/app/util/Solr.scala
@@ -1,3 +1,7 @@
+package util.plugins
+
+import models.{Asset, AssetMetaValue, MetaWrapper}
+
 import org.apache.solr.client.solrj._
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer
 import org.apache.solr.core.CoreContainer
@@ -30,4 +34,57 @@ object Solr {
 
   def query(q: SolrQuery) = Nil
 
+  type AssetSolrDocument = Map[String, SolrValue]
+
 }
+import Solr.AssetSolrDocument
+
+sealed trait SolrValue
+
+trait SolrSingleValue extends SolrValue
+case class SolrIntValue(value: Int) extends SolrSingleValue
+case class SolrStringValue(value: String) extends SolrSingleValue
+case class SolrBooleanValue(value: Boolean) extends SolrSingleValue
+
+case class SolrMultiValue(values: Seq[SolrSingleValue]) extends SolrValue {
+  def +(v: SolrSingleValue) = this.copy(values = values :+ v)
+}
+
+
+trait AssetSolrSerializer {
+  def serialize(asset: Asset): AssetSolrDocument
+}
+
+/**
+ * asset meta values are all converted into strings with the meta name as the
+ * solr key, using group_id to group values in to multi-valued keys
+ */
+class FlatSerializer extends AssetSolrSerializer {
+
+  def serialize(asset: Asset) = Map[String, SolrValue](
+    "tag" -> SolrStringValue(asset.tag),
+    "status" -> SolrStringValue(asset.getStatusName()),
+    "assetType" -> SolrIntValue(asset.getType.id)
+  ) ++ serializeMetaValues(AssetMetaValue.findByAsset(asset))
+
+  def serializeMetaValues(values: Seq[MetaWrapper]) = {
+    def process(build: AssetSolrDocument, remain: Seq[MetaWrapper]): AssetSolrDocument = remain match {
+      case head :: tail => {
+        val newval = SolrStringValue(head.getValue())
+        val mergedval = build.get(head.getName()) match {
+          case Some(exist) => exist match {
+            case s: SolrSingleValue => SolrMultiValue(s :: newval :: Nil)
+            case m: SolrMultiValue => m + newval
+          }
+          case None => newval
+        }
+        process(build + (head.getName() -> mergedval), tail)
+      }
+      case _ => build
+    }
+    process(Map(), values)
+  }
+
+}
+
+
