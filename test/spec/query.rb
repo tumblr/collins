d9cccd1445bc99963ad472b8a33bcbdece1e1d51
Dan Simon <dan@tumblr.com>
2012-08-01 13:59:37 -0400

    refactoring query rspec tests

--- test/spec/query.rb
+++ test/spec/query.rb
@@ -2,6 +2,15 @@
 require 'collins_client'
 
 describe "Asset Search" do
+
+  def checkTags query, expectedTags
+    assets = @client.search query
+    tags = assets.map do |asset|
+      asset.tag
+    end
+    tags.should eql expectedTags
+  end
+    
   
   before :all do
     config = {:username => "blake", :password => "admin:first", :host => "http://127.0.0.1:9000"}
@@ -9,41 +18,23 @@ describe "Asset Search" do
   end
 
   it "simple tag query" do
-    assets = @client.search "tag = tumblrtag1"
-    assets.size.should eql 1
+    checkTags "tag = tumblrtag1", ["tumblrtag1"]
   end
 
   it "hostname exact match" do
-    assets = @client.search "hostname = web-6ec32d2e.ewr01.tumblr.net"
-    assets.size.should eql 1
-    assets[0].tag.should eql "001016"
+    checkTags  "hostname = web-6ec32d2e.ewr01.tumblr.net",  ["001016"]
   end
 
   it "simple or" do 
-    assets = @client.search 'hostname = web-6ec32d2e.ewr01.tumblr.net OR tag = "000981"'
-    assets.size.should eql 2
-    tags = assets.map do |asset|
-      asset.tag
-    end
-    tags.should eql ["000981", "001016"]
+    checkTags  'hostname = web-6ec32d2e.ewr01.tumblr.net OR tag = "000981"', ["000981", "001016"]
   end
 
   it "simple and" do
-    assets = @client.search 'hostname = web-6ec32d2e.ewr01.tumblr.net AND tag = "001016"'
-    assets.size.should eql 1
-    tags = assets.map do |asset|
-      asset.tag
-    end
-    tags.should eql [ "001016" ] 
+    checkTags  'hostname = web-6ec32d2e.ewr01.tumblr.net AND tag = "001016"', [ "001016" ]
   end
 
   it "asset type" do
-    assets = @client.search 'type = configuration'
-    assets.size.should eq 12
-    tags = assets.map do |asset|
-      asset.tag
-    end
-    tags.should eql [
+    checkTags  'type = configuration', [
       "Tumblr",
       "dns_ewr01_tumblr_net",
       "ewr01haproxy_generation",
@@ -68,12 +59,7 @@ describe "Asset Search" do
   end
 
   it "status and type and not" do
-    assets = @client.search 'type = configuration AND NOT status = incomplete'
-    assets.size.should eq 6
-    tags = assets.map do |asset|
-      asset.tag
-    end
-    tags.should eql [
+    checkTags  'type = configuration AND NOT status = incomplete', [
       "nodeclassifer_web_10g",
       "nodeclassifier_compute",
       "nodeclassifier_io",
