f938f0edeeadf7cb9b576aa90745638e27e50895
Dan Simon <dan@tumblr.com>
2012-08-17 16:51:13 -0400

    new version router for routing based on API version

---
+++ app/util/VersionRouter.scala
@@ -0,0 +1,41 @@
+package util
+
+import play.api.mvc.{Request, Headers}
+import controllers.actions.SecureAction
+
+
+/**
+ * This enumeration only exists to help keep versions consistent, so we don't
+ * accidently route for non-existant versions
+ */
+object ApiVersion extends Enumeration {
+  type ApiVersion = Value
+  val `1.0` = Value("1.0")
+  val `2.0` = Value("2.0")
+  val `2.1` = Value("2.1")
+
+  def safeWithName(name: String): Option[ApiVersion] = try Some(withName(name)) catch {case _ => None}
+}
+import ApiVersion._
+
+object VersionRouter {
+
+  val acceptHeader = """com.tumblr.collins;version=([0-9]+\.[0-9]+)""".r
+
+  def apply[T](routes: Map[ApiVersion, T])(requestHeaders: Headers): T = requestHeaders
+    .toMap
+    //get the accept header
+    .get("Accept").toRight("Missing required Accept header").right
+    //match the regex
+    .flatMap{_.collectFirst{case acceptHeader(version) => version}.toRight("invalid Accept header")}.right
+    //verify the version is valid
+    .flatMap{version => ApiVersion.safeWithName(version).toRight("Unknown Version " + version)}.right
+    //find the route for the version
+    .flatMap{v => routes.get(v).toRight("Unrouted version " + v.toString)}
+    .fold(
+      err => throw new Exception(err),
+      route => route
+    )
+
+}
+
---
+++ test/util/VersionRouterSpec.scala
@@ -0,0 +1,39 @@
+package util
+
+import play.api.mvc.Headers
+
+import ApiVersion._
+
+import org.specs2._
+
+class VersionRouterSpec extends mutable.Specification {
+
+  val map = Map(
+    `2.0` -> "A",
+    `2.1` -> "B"
+  )
+
+  case class FakeHeaders(headers: Map[String, Seq[String]]) extends Headers {
+    def getAll(key: String) = headers(key)
+    def keys = headers.keys.toSet
+  }
+
+  "version router" should {
+    "route to correct version" in {
+      val heads = FakeHeaders(Map("Accept" -> List("com.tumblr.collins;version=2.1", "foo", "com.tumblr.collins")))
+      VersionRouter(map)(heads) must_== "B"
+
+    }
+    "throw exception on missing header" in {
+      VersionRouter(map)(FakeHeaders(Map[String, Seq[String]]())) must throwA[Exception]
+    }
+    "throw exception on malformed header" in {
+      VersionRouter(map)(FakeHeaders(Map("Accept" -> List("HASFIAFSHAF")))) must throwA[Exception]
+    }
+    "throw exception on invalid version" in {
+      val heads = FakeHeaders(Map("Accept" -> List("com.tumblr.collins;version=26.12", "foo", "com.tumblr.collins")))
+      VersionRouter(map)(heads) must throwA[Exception]
+    }
+  }
+
+}
