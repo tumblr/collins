9803f81a5eca50d4c57a2c9e9915a5131a3f9af9
Blake Matheny <bmatheny@mobocracy.net>
2012-09-01 14:41:10 -0400

    Moved decorator code to use new configs

--- app/util/views/DecoratorParser.scala
+++ app/util/views/DecoratorParser.scala
@@ -1,22 +1,20 @@
 package util
 package views
 
-import play.api.Configuration
-
 trait DecoratorParser {
-  def apply(key: String, config: Configuration): DecoratorParser
+  def apply(key: String, config: DecoratorConfig): DecoratorParser
   def parse(string: String): Seq[String]
 }
 
 class IdentityParser extends DecoratorParser {
-  def apply(key: String, config: Configuration) = this
+  def apply(key: String, config: DecoratorConfig) = this
   def parse(string: String): Seq[String] = Seq(string)
 }
 
 class DelimiterParser(delimiter: String) extends DecoratorParser {
   def this() = this(" ")
-  def apply(key: String, config: Configuration) = {
-    val d = config.getString("delimiter").getOrElse {
+  def apply(key: String, config: DecoratorConfig) = {
+    val d = config.delimiter.getOrElse {
       throw DecoratorConfigurationException(key, "delimiter")
     }
     new DelimiterParser(d)
