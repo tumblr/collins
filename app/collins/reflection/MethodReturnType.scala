9c79df183922d9295b13164cc011074c8e9ec37c
Blake Matheny <bmatheny@mobocracy.net>
2012-09-03 12:39:46 -0400

    Pulled common reflection code from callback package into reflection package. Closes #8

---
+++ app/collins/reflection/MethodReturnType.scala
@@ -0,0 +1,9 @@
+package collins
+package reflection
+
+import java.lang.reflect.Method
+
+trait MethodReturnType {
+  def isBooleanReturnType(method: Method): Boolean =
+    method.getReturnType.equals(java.lang.Boolean.TYPE)
+}
