b2ea22fe7b92903392df72b903bc34f4b601d306
Blake Matheny <bmatheny@mobocracy.net>
2012-09-06 18:28:58 -0400

    Prepping code for supporting reintake re #12
    Fixed a couple of bugs:
    
      * Reloading a JProfile in dev was broken due to different classloaders
      * Fixed a bug in auth where upper case group names weren't properly handled
    
    Also added support for features.sloppyTags, which allows any tags to be
    completely managed outside of collins. Things in general will not work well by
    doing this but can be useful for testing or one off situations.

--- app/src/main/java/com/tumblr/play/interop/JProvisionerProfile.java
+++ app/src/main/java/com/tumblr/play/interop/JProvisionerProfile.java
@@ -2,6 +2,7 @@ package com.tumblr.play.interop;
 
 import java.util.Map;
 
-public class JProvisionerProfile {
+public class JProvisionerProfile implements java.io.Serializable {
+    static final long serialVersionUID = -8425566388153807286L;
     public Map<String, JProfile> profiles;
 }
