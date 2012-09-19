c02581b0bda9ec6914d0320c676da9a13b22825d
Blake Matheny <bmatheny@mobocracy.net>
2012-09-19 15:33:09 -0400

    Use labels for asset types when we have them

--- conf/evolutions/collins/10.sql
+++ conf/evolutions/collins/10.sql
@@ -4,7 +4,7 @@
 
 ALTER TABLE asset_type ADD COLUMN label VARCHAR(255) NOT NULL DEFAULT '';
 UPDATE asset_type SET name='SERVER_NODE', label='Server Node' WHERE id=1;
-UPDATE asset_type SET name='SERVER_CHASSIS', label='Server Node' WHERE id=2;
+UPDATE asset_type SET name='SERVER_CHASSIS', label='Server Chassis' WHERE id=2;
 UPDATE asset_type SET name='RACK', label='Rack' WHERE id=3;
 UPDATE asset_type SET name='SWITCH', label='Switch' WHERE id=4;
 UPDATE asset_type SET name='ROUTER', label='Router' WHERE id=5;
@@ -17,7 +17,7 @@ UPDATE asset_type SET name='CONFIGURATION', label='Configuration' WHERE id=9;
 
 ALTER TABLE asset_type DROP COLUMN label;
 UPDATE asset_type SET name='Server Node' WHERE id=1;
-UPDATE asset_type SET name='Server Node' WHERE id=2;
+UPDATE asset_type SET name='Server Chassis' WHERE id=2;
 UPDATE asset_type SET name='Rack' WHERE id=3;
 UPDATE asset_type SET name='Switch' WHERE id=4;
 UPDATE asset_type SET name='Router' WHERE id=5;
