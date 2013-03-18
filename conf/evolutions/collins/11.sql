f30265777e6a23e56396b0dde733f8dcdacf76bb
cburroughs <chris.burroughs@gmail.com>
2013-03-18 09:59:13 -0400

    ensure that new basic lshw fields are in the DB at start, and thus displayed in the default search index

---
+++ conf/evolutions/collins/11.sql
@@ -0,0 +1,6 @@
+# --- Include new common server base fields
+
+# --- !Ups
+
+INSERT INTO asset_meta VALUES (0, 'BASE_PRODUCT', 1, 'Base Product', 'Formal product model designation', 1);
+INSERT INTO asset_meta VALUES (0, 'BASE_VENDOR', 1, 'Base Vendor', 'Who made your spiffy computer?', 1);
