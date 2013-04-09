6eba25d87ffb3e2b7c111c111b423b0aef96e744
cburroughs <chris.burroughs@gmail.com>
2013-04-09 10:28:32 -0400

    explicitly include BASE_DESCRIPTION at negative priority

--- conf/evolutions/collins/11.sql
+++ conf/evolutions/collins/11.sql
@@ -4,3 +4,4 @@
 
 INSERT INTO asset_meta VALUES (0, 'BASE_PRODUCT', 1, 'Base Product', 'Formal product model designation', 1);
 INSERT INTO asset_meta VALUES (0, 'BASE_VENDOR', 1, 'Base Vendor', 'Who made your spiffy computer?', 1);
+INSERT INTO asset_meta VALUES (0, 'BASE_DESCRIPTION', -1, 'Base Description', 'How does your computer introduce itself?', 1);
