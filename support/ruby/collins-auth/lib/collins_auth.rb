befafe5a82358fbb087e66bce0349e96de40d54f
Gabe Conradi <gabe@tumblr.com>
2014-07-07 15:10:56 -0400

    syntax linters dont like this

--- support/ruby/collins-auth/lib/collins_auth.rb
+++ support/ruby/collins-auth/lib/collins_auth.rb
@@ -5,7 +5,7 @@ require 'socket'
 
 module Collins
   module Authenticator
-    def self.setup_client(options = {prompt: false})
+    def self.setup_client(options = {:prompt => false})
       if options[:config_file] and not File.readable? options[:config_file]
         raise 'unable to read invalid config file: ' + options[:config_file]
       end
