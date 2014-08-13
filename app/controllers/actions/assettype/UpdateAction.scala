dee6d074758cbe4101ec7b7990ba6f543a8dc2d5
Gabe Conradi <gabe@tumblr.com>
2014-08-13 14:52:16 -0400

    fix incorrect curl

--- app/controllers/actions/assettype/UpdateAction.scala
+++ app/controllers/actions/assettype/UpdateAction.scala
@@ -31,7 +31,7 @@ import play.api.data.Forms._
  *  collins-shell asset_type update OLDNAME [--name=NAME --label=LABEL]
  * }}}
  * @curlexample {{{
- *  curl -v -u blake:admin:first --basic \
+ *  curl -XPOST -u blake:admin:first --basic \
  *    -d name='NEW_NAME' \
  *    http://localhost:9000/api/assettype/OLD_NAME
  * }}}
