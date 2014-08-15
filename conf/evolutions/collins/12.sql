b84cbb5b43b369214eeed94d5966fc3fcbf1e056
Gabe Conradi <gabe@tumblr.com>
2014-08-15 14:47:12 -0400

    change priority to highest to order towards the top

--- conf/evolutions/collins/12.sql
+++ conf/evolutions/collins/12.sql
@@ -3,7 +3,7 @@
 # --- !Ups
 
 INSERT INTO asset_meta (name, priority, label, description) VALUES ('HOSTNAME', 0, 'Hostname', 'Hostname of asset');
-INSERT INTO asset_meta (name, priority, label, description) VALUES ('NODECLASS', 1, 'Nodeclass', 'Nodeclass of asset');
-INSERT INTO asset_meta (name, priority, label, description) VALUES ('POOL', 2, 'Pool', 'Pool');
-INSERT INTO asset_meta (name, priority, label, description) VALUES ('PRIMARY_ROLE', 3, 'Primary Role', 'Primary role of host or asset');
-INSERT INTO asset_meta (name, priority, label, description) VALUES ('SECONDARY_ROLE', 4, 'Secondary Role', 'Secondary role of host or asset');
+INSERT INTO asset_meta (name, priority, label, description) VALUES ('NODECLASS', 0, 'Nodeclass', 'Nodeclass of asset');
+INSERT INTO asset_meta (name, priority, label, description) VALUES ('POOL', 0, 'Pool', 'Pool');
+INSERT INTO asset_meta (name, priority, label, description) VALUES ('PRIMARY_ROLE', 0, 'Primary Role', 'Primary role of host or asset');
+INSERT INTO asset_meta (name, priority, label, description) VALUES ('SECONDARY_ROLE', 0, 'Secondary Role', 'Secondary role of host or asset');
