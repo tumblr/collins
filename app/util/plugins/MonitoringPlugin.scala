5ef009740c1e1954253124d86506ef4bb8056ff5
Gabe Conradi <gabe@tumblr.com>
2014-08-18 13:22:50 -0400

    whoops, remove debug println

--- app/util/plugins/MonitoringPlugin.scala
+++ app/util/plugins/MonitoringPlugin.scala
@@ -4,7 +4,6 @@ import play.api.Play
 
 object MonitoringPlugin {
   def option(): Option[collins.monitoring.MonitoringPlugin] = {
-    println("-----------------------")
     Play.maybeApplication.flatMap { app =>
       app.plugin[collins.monitoring.MonitoringPlugin].filter(_.enabled)
     }
