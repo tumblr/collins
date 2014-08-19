d1817e018bd9e98a2ef79bc64d68cb808cce2864
Gabe Conradi <gabe@tumblr.com>
2014-08-19 12:36:32 -0400

    update language for roles and pools

--- conf/evolutions/collins/12.sql
+++ conf/evolutions/collins/12.sql
@@ -4,6 +4,6 @@
 
 INSERT INTO asset_meta (name, priority, label, description) VALUES ('HOSTNAME', 0, 'Hostname', 'Hostname of asset');
 INSERT INTO asset_meta (name, priority, label, description) VALUES ('NODECLASS', 0, 'Nodeclass', 'Nodeclass of asset');
-INSERT INTO asset_meta (name, priority, label, description) VALUES ('POOL', 0, 'Pool', 'Pool');
-INSERT INTO asset_meta (name, priority, label, description) VALUES ('PRIMARY_ROLE', 0, 'Primary Role', 'Primary role of host or asset');
-INSERT INTO asset_meta (name, priority, label, description) VALUES ('SECONDARY_ROLE', 0, 'Secondary Role', 'Secondary role of host or asset');
+INSERT INTO asset_meta (name, priority, label, description) VALUES ('POOL', 0, 'Pool', 'Groups related assets spanning multiple functional roles');
+INSERT INTO asset_meta (name, priority, label, description) VALUES ('PRIMARY_ROLE', 0, 'Primary Role', 'Primary functional role of asset');
+INSERT INTO asset_meta (name, priority, label, description) VALUES ('SECONDARY_ROLE', 0, 'Secondary Role', 'Secondary functional role of asset');
