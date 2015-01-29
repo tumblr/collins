b202e341cc7c4d9cfa73bb9b247603470b9c1f0a
Bhaskar Maddala <bhaskar@tumblr.com>
2015-01-29 08:05:44 -0500

    STEP 7: Promise and BackgroundProcess updates.
    Summary:
    Update the API for Promise and Timeouts.
    NOTE: Requires a closer review

--- app/util/concurrent/BackgroundProcess.scala
+++ app/util/concurrent/BackgroundProcess.scala
@@ -3,9 +3,10 @@ package concurrent
 
 import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
 
 trait BackgroundProcess[T] {
-  val timeout: Duration
+  val timeout: FiniteDuration
   def run(): T
 
   protected def defaultTimeout: Duration = Duration(ConcurrencyConfig.timeoutMs, TimeUnit.MILLISECONDS)
--- app/util/concurrent/BackgroundProcessor.scala
+++ app/util/concurrent/BackgroundProcessor.scala
@@ -1,6 +1,7 @@
 package util
 package concurrent
 
+import akka.util.Timeout.durationToTimeout
 import akka.actor._
 import akka.pattern.ask
 import akka.routing.RoundRobinRouter
@@ -9,7 +10,8 @@ import scala.concurrent.duration.Duration
 import play.api.libs.concurrent._
 
 import java.util.concurrent.TimeoutException
-//import scala.collection.immutable.Vector
+import play.api.libs.concurrent.Execution.Implicits._
+
 
 class BackgroundProcessorActor extends Actor {
   def receive = {
@@ -36,8 +38,8 @@ object BackgroundProcessor {
 
   type SendType[T] = Tuple2[Option[Throwable], Option[T]]
   def send[PROC_RES,RESPONSE](cmd: BackgroundProcess[PROC_RES])(result: SendType[PROC_RES] => RESPONSE)(implicit mf: Manifest[PROC_RES]) = {
-    ask(ref, cmd)(cmd.timeout).mapTo[PROC_RES].asPromise.extend1 {
-      case Redeemed(v) => result(Tuple2(None, Some(v)))
+    ask(ref, cmd)(cmd.timeout).mapTo[PROC_RES].map {
+      case Redeemed(v: PROC_RES) => result(Tuple2(None, Some(v)))
       case Thrown(e) => e match {
         case t: TimeoutException =>
           result(Tuple2(Some(SexyTimeoutException(cmd.timeout)), None))
