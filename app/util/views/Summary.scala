759c3254006cc16c88401ed602484862758fed10
Gabe Conradi <gabe@tumblr.com>
2015-01-30 16:02:52 -0500

    add status:state label on asset show pages

--- app/util/views/Summary.scala
+++ app/util/views/Summary.scala
@@ -2,6 +2,7 @@ package util
 package views
 
 import models.Asset
+import models.Status
 
 import play.api.mvc.Content
 import play.api.templates.Html
@@ -29,7 +30,8 @@ trait Summarizer[T] {
 case class AssetSummary(template: String) extends Summarizer[Asset] {
   override val replacementMap = Map(
     "assetType" -> getAssetType _,
-    "hostname" -> getHostname _
+    "hostname" -> getHostname _,
+    "statusLabel" -> getStatusLabel _
   )
 
   def getAssetType(asset: Asset): String = {
@@ -38,6 +40,27 @@ case class AssetSummary(template: String) extends Summarizer[Asset] {
       case false => asset.getType().label
     }
   }
+  def getStatusLabel(asset: Asset): String = {
+    val state_string = asset.getState match {
+      case Some(st) => ":%s" format st.name
+      case None => ""
+    }
+    val state_descr = asset.getState match {
+      case Some(st) => " - %s" format st.description
+      case None => ""
+    }
+    val badge_class = asset.getStatus.name match {
+      case "Allocated"                    => "label-primary"
+      case "Cancelled" | "Decommissioned" => "label-default"
+      case "Maintenance"                  => "label-danger"
+      case "Incomplete" | "New"           => "label-warning"
+      case "Provisioned" | "Provisioning" => "label-info"
+      case "Unallocated"                  => "label-success"
+      case _                              => "label-default"
+    }
+    "<span data-rel=\"tooltip\" data-original-title=\"" + asset.getStatus.description + state_descr +
+      "\" data-placement=\"bottom\"  class=\"label " + badge_class + "\">" + asset.getStatusName + state_string + "</span>"
+  }
   def getHostname(asset: Asset): String = {
     asset.getMetaAttribute("HOSTNAME").map(_.getValue).getOrElse(asset.tag)
   }
@@ -45,7 +68,7 @@ case class AssetSummary(template: String) extends Summarizer[Asset] {
 
 object Summary {
   def apply(asset: Asset): Content = {
-    val template = "<h1>{assetType} <small>{hostname}</small></h1>"
+    val template = "<h1>{assetType} <small>{hostname}</small></h1><h4 class=\"asset-status-label\">{statusLabel}</h4>"
     AssetSummary(template).get(asset)
   }
 }
