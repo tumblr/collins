cf0f5396ad8bb69d54d637059a0bfa111e1cd30c
Bhaskar Maddala <bhaskar@tumblr.com>
2015-01-31 21:51:38 -0500

    STEP 22: Update travis and bump versions

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "collins"
-    val appVersion      = "1.2.5"
+    val appVersion      = "1.4.0"
 
     val appDependencies = Seq()
 
