8fd30c84cffa7dfe71761a79958249da161d94ab
Bhaskar Maddala <bhaskar@tumblr.com>
2015-03-26 05:01:23 -0400

    Move all timeouts to external config
    Summary:
    Eliminating the use of hardcoded configs. The only timeout on in
    config is TestProcessor which is a testing endpoint for api/asyncPing.
    
    Please review the conf/* files including the reference files and
    ActivationProcessor and AssetCancelProcessor which take config
    values from SoftlayerPlugin

--- app/controllers/actors/ActivationProcessor.scala
+++ app/controllers/actors/ActivationProcessor.scala
@@ -6,10 +6,11 @@ import play.api.mvc.{AnyContent, Request}
 import util.concurrent.BackgroundProcess
 import util.plugins.SoftLayer
 import scala.concurrent.{Await, Future}
+import java.util.concurrent.TimeUnit
+import collins.softlayer.SoftLayerConfig
 
 case class ActivationProcessor(slId: Long, userTimeout: Option[FiniteDuration] = None)(implicit req: Request[AnyContent]) extends BackgroundProcess[Boolean] {
-  override def defaultTimeout = 60 seconds
-  val timeout = userTimeout.getOrElse(defaultTimeout)
+  val timeout = userTimeout.getOrElse(Duration(SoftLayerConfig.activationRequestTimeoutMs, TimeUnit.MILLISECONDS))
 
   def run(): Boolean = {
     val plugin = SoftLayer.pluginEnabled.get
