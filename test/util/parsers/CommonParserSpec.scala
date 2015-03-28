fa5600d5a7fa8c615ae97921b8fc8f353cf5a990
Bhaskar Maddala <bhaskar@tumblr.com>
2015-03-28 15:08:55 -0400

    Making the tests run all of them

--- test/util/parsers/CommonParserSpec.scala
+++ test/util/parsers/CommonParserSpec.scala
@@ -11,16 +11,8 @@ import specification._
 trait CommonParserSpec[REP] extends ResourceFinder {
 
   def getParser(txt: String): _root_.util.parsers.CommonParser[REP]
-  def getParseResults(filename: String, options: Map[String,String] = Map.empty): Either[Throwable,REP] = {
+  def getParseResults(filename: String): Either[Throwable,REP] = {
     val data = getResource(filename)
-    if (options.nonEmpty) {
-      val cfg = Configuration.from(options)
-      _root_.util.config.AppConfig.globalConfig = Some(cfg);
-    } else {
-      _root_.util.config.AppConfig.globalConfig = None;
-    }
-    LshwConfig.initialize
-    LldpConfig.initialize
     val parser = getParser(data)
     parser.parse()
   }
