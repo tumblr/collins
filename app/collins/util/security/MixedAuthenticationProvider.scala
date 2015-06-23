057b6cfba7703dfd764b1d8fa430a2d9b757026b
Bhaskar Maddala <bhaskar@tumblr.com>
2015-06-23 07:50:47 -0400

    Use the tryAuthCache method to avoid making auth queries continously.

--- app/collins/util/security/MixedAuthenticationProvider.scala
+++ app/collins/util/security/MixedAuthenticationProvider.scala
@@ -29,20 +29,20 @@ class MixedAuthenticationProvider(types: Array[String]) extends AuthenticationPr
       case "default" => {
         logger.trace("mock authentication type")
         val defaultProvider = AuthenticationProvider.Default
-        val user = defaultProvider.authenticate(username, password)
+        val user = defaultProvider.tryAuthCache(username, password)
         logger.debug("Tried mock authentication for %s, got back %s".format(username, user))
         user
       }
       case "file" => {
         logger.trace("file authentication type")
         val fileProvider = new FileAuthenticationProvider()
-        val user = fileProvider.authenticate(username, password)
+        val user = fileProvider.tryAuthCache(username, password)
         logger.debug("Tried file authentication for %s, got back %s".format(username, user))
         user
       }
       case "ldap" => {
         val ldapProvider = new LdapAuthenticationProvider()
-        val user = ldapProvider.authenticate(username, password)
+        val user = ldapProvider.tryAuthCache(username, password)
         logger.debug("Tried ldap authentication for %s, got back %s".format(username, user))
         user
       }
