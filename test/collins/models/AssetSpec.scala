370d4d5160c80104ca4370485ea3d1fb72bab8ce
Bhaskar Maddala <bhaskar@tumblr.com>
2015-10-04 13:02:11 -0400

    Reenable the asset distance sorter nodeclass similarity test case.

--- test/collins/models/AssetSpec.scala
+++ test/collins/models/AssetSpec.scala
@@ -38,54 +38,6 @@ class AssetSpec extends mutable.Specification {
       }.getOrElse(failure("Couldn't find asset but expected to"))
     }
 
-//    "Support nodeclass" in new WithApplication(FakeApplication(
-//      additionalConfiguration = Map(
-//        "solr.enabled" -> true,
-//        "solr.repopulateOnStartup" -> true))) {
-//
-//      def createAssetMetas(asset: Asset, metamap: Map[String, String]) = metamap
-//        .map {
-//          case (k, v) =>
-//            AssetMetaValue.create(AssetMetaValue(asset.id, AssetMeta.findOrCreateFromName(k).id, 0, v))
-//        }
-//      val nodeclassTag = "test_nodeclass"
-//      val nodeclassStatus = Status.Allocated.get
-//      val nodeclassType = AssetType.Configuration.get
-//      val nodeclassIdentifierTag = ("IS_NODECLASS" -> "true")
-//      val nodeclassMetaTags = Map("FOOT1" -> "BAR", "BAZT1" -> "BAAAAZ")
-//      val assetTag = "nodeclasstest"
-//      val assetStatus = Status.Allocated.get
-//      val assetType = AssetType.ServerNode.get
-//      val similarAssetTag = "similar_asset"
-//      val similarAssetData = List[(String, Status, Map[String, String])](
-//        (similarAssetTag, Status.Unallocated.get, nodeclassMetaTags),
-//        ("not_similar", Status.Unallocated.get, Map[String, String]()),
-//        ("similar_not_unallocated", Status.Provisioned.get, nodeclassMetaTags))
-//
-//      val nodeclass = Asset.create(Asset(nodeclassTag, nodeclassStatus, nodeclassType))
-//      val testAsset = Asset.create(Asset(assetTag, assetStatus, assetType))
-//      val nodeclassMetas = createAssetMetas(nodeclass, (nodeclassMetaTags + nodeclassIdentifierTag))
-//      val assetMetas = createAssetMetas(testAsset, nodeclassMetaTags)
-//      testAsset.nodeClass must_== Some(nodeclass)
-//
-//      val assets = similarAssetData.map {
-//        case (tag, status, metatags) => {
-//          val asset = Asset.create(Asset(tag, status, AssetType.ServerNode.get))
-//          createAssetMetas(asset, metatags)
-//          asset
-//        }
-//      }
-//      val finder = AssetFinder.empty.copy(
-//        status = Status.Unallocated,
-//        assetType = Some(AssetType.ServerNode.get))
-//      val expected = assets.filter { _.tag == similarAssetTag }
-//      val page = PageParams(0, 10, "", "tag")
-//      Asset.findByTag(assetTag).map { asset =>
-//        Asset.findSimilar(asset, page, finder, AssetSort.Distribution).items must_== expected
-//      }.getOrElse(failure("Couldn't find asset but expected to"))
-//
-//    } //support nodeclass
-
     "Support getters/finders" in new WithApplication {
 
       val assetTag = "tumblrtag1"
