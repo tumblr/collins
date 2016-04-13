4e3687c716857288182bdd004681319ed7a502d3
davidblum <david.blum@sendgrid.com>
2016-04-13 14:26:32 -0600

    add /usr array of java_home locations

--- scripts/collins.sh
+++ scripts/collins.sh
@@ -64,7 +64,7 @@ function find_java() {
   if [ ! -z "$JAVA_HOME" ]; then
     return
   fi
-  for dir in /opt/jdk /System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home /usr/java/default; do
+  for dir in /opt/jdk /System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home /usr/java/default /usr; do
     if [ -x $dir/bin/java ]; then
       JAVA_HOME=$dir
       break
