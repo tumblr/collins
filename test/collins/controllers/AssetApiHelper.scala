0e56e5041e9f6e2fc6742a63f4eff0c98e90a504
Michael Schuett <michaeljs1990@gmail.com>
2017-05-21 03:19:36 -0400

    Parameterize LLDP XML
    Allow for optionally overwritting the LLDP XML used when updating
    an asset for testing.

--- test/collins/controllers/AssetApiHelper.scala
+++ test/collins/controllers/AssetApiHelper.scala
@@ -54,10 +54,10 @@ trait AssetApiHelper extends ResponseMatchHelpers with JsonMatchers with Control
     result
   }
 
-  def updateHwInfo(lshwXml: String = "lshw-basic.xml") = {
+  def updateHwInfo(lshwXml: String = "lshw-basic.xml", lldpXml: String = "lldpctl-two-nic.xml") = {
     // update hw details (lshw / lldp) - cannot proceed without this
     val lshwData = getResource(lshwXml)
-    val lldpData = getResource("lldpctl-two-nic.xml")
+    val lldpData = getResource(lldpXml)
     val dummy = Seq[FilePart[TemporaryFile]]()
     val mdf = MultipartFormData(Map(
       "lshw" -> Seq(lshwData),
