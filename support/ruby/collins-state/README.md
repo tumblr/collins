f658b3e61a65add39a9ef729bad5e801e6c22945
Felix Aronsson <felix@tumblr.com>
2018-03-15 13:53:49 +0100

    Deprecate collins_state

--- support/ruby/collins-state/README.md
+++
@@ -1,6 +0,0 @@
-# collins_state
-
-Collins state is a simple state management framework built on top of collins.
-
-Take a look at some of the specs or the provisioning workflow for some sample
-code.
--- support/ruby/collins-state/lib/collins_state.rb
+++
@@ -1,4 +0,0 @@
-$:.unshift File.join File.dirname(__FILE__)
-require 'collins_client'
-require 'collins/persistent_state'
-Dir[File.join(File.dirname(__FILE__), 'collins', 'workflows', '*.rb')].each {|f| require f}
